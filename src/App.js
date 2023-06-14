932ac41c1902b4952a13071911c3474398b6e247
Saurabh kumar <73513169+saurabh2232@users.noreply.github.com>
2023-06-14 18:27:57 +0530

    Registrationform

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import FormInput from "./Components/FormInput";
+const App = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: ""
+  });
 
-function App() {
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "User Name",
+      errorMessage:
+        "Username should be 3-16 characters and should not include any special characters! like: @, #, $, %, *, ., -,/ etc. ",
+      label: "User Name",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      errorMessage: "You must be 12 years old or above to register.",
+      pattern: "",
+      label: "Birthday",
+      required: true
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: " Current Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character for example: Abcd123@",
+      label: "Password",
+      pattern:
+        "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$",
+      required: true
+    },
+    {
+      id: 5,
+      type: "password",
+      name: "confirmPassword",
+      placeholder: "confirm Password",
+      errorMessage: "Password does not match!",
+      label: "confirm Password",
+      pattern: values.password,
+      required: true
+    }
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+  console.log(values);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
